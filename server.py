155f7da900134c22e42937a5fab9f6d161429240
Notme-yep <85176233+Notme-yep@users.noreply.github.com>
2021-06-10 13:45:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def hello_world():
-#     return render_template("index.html")
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
 
 
 @app.route('/<string:page_name>')
