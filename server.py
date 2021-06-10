a99dd26fe1ebdb6691aec17562c945a6d738c6e1
Notme-yep <85176233+Notme-yep@users.noreply.github.com>
2021-06-10 12:58:54 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return "GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG"
+#     return render_template("index.html")
 
 
 # @app.route('/<string:page_name>')
